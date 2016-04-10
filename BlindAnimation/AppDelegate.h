e0a9d221f41dfdc0f88c8a5f84a48cc13e6996af
HGDQ <zhuming3834@sina.com>
2016-04-10 13:15:43 +0800

    Initial Commit

---
+++ BlindAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlindAnimation
+//
+//  Created by hgdq on 16/4/10.
+//  Copyright © 2016年 hgdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
